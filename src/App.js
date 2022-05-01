b7a016d08d4595b7dcbdc429c46c17f89a5a2fa1
Zia Ullah Sarwar <ziasarwar20297@gmail.com>
2022-05-01 23:43:57 +0500

    Hello World

--- src/App.js
+++ src/App.js
@@ -16,7 +16,15 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+
         </a>
+        <form action="/action.php">
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" value="Mike"><br><br>
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" value="Walker"><br><br>
+  <input type="submit" value="Submit">
+</form>
       </header>
     </div>
   );
